3161737579f34c04341737a349d9b52408439771
Abhishek Anand <abhishekrajput301999@gmail.com>
2026-03-08 22:50:42 +0530

    Count digits in number

---
+++ CountDigits.java
@@ -0,0 +1,19 @@
+public class CountDigits {
+
+    public static void main(String[] args) {
+
+        int number = 12345;
+        int count = 0;
+
+        while(number > 0) {
+
+            number = number / 10;
+            count++;
+
+        }
+
+        System.out.println("Total digits: " + count);
+
+    }
+
+}
